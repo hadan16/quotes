772af49a11101ab613e1a00c87d565ec7738cb68
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-02-21 15:06:14 -0800

    forEach'd the data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(document).on('ready', function() {
     // js link check
     console.log("app.js linked.");
 
-    //for use with helper function below used to reder all posts to view
+    //for use with helper function below used to render all posts to view
     $quotesList = $('#quoteTarget');
 
     $(function(){
@@ -28,27 +28,90 @@ var data;
 
 
 // function that runs after ajax call
-function onSuccess(responseData){
-    // console.log(responseData);
-    data = responseData;
+function onSuccess(data){
+    console.log(data.quotes.length);
 
-    // IS DATA FROM THE DATABASE APPEARING IN THE DOM??
-    $("body").text(data.quotes[0].phrase);
+
+    // BEGIN LOOPING
+
+    data.quotes.forEach(function(index){
+      console.log(index.phrase);
+      $("#quoteTarget").append(
+
+          `<div class="row result">
+
+                  <div class="song-artist">
+                    <h3>${index.phrase}</h3>
+                    <h4>${index.author}</h4>
+                  </div>
+
+          </div>  <!-- -row-result close -->
+
+          <hr>`
+
+
+
+      )
+    })
+  // for (var i=0; i<data.quotes.length; i++){
+  //
+  //
+  //   phrase = data.quotes[i].phrase;
+  //   author = data.quotes[i].author;
+  //
+  //   var varNewSection = `<div class="row result">
+  //
+  //           <div class="song-artist">
+  //             <h3 id="artist-${[i]}">Quote</h3>
+  //             <h4 id="track-${[i]}">Author</h4>
+  //           </div>
+  //
+  //       </div>  <!-- -row-result close -->
+  //
+  //       <hr>`;
+  //
+  //   function appendHtml(){
+  //   $(".container").append(varNewSection);
+  //   }
+  //   appendHtml();
+  //
+  //   function appendPhrase(){
+  //     $(`#artist-${i}`).text(phrase);
+  //   };
+  //   appendPhrase();
+  //
+  //   function appendAuthor(){
+  //     $(`#track-${i}`).text(author);
+  //   };
+  //   appendAuthor();
+
+
+   //end loop
+
+
+
+
+
+
+
+
+
+
 };//end onSuccess function
 
 
 
-// helper function to render all posts to view. note: we empty and re-render the collection each time our post data changes
-function render () {
-  // empty existing posts from view
-  $quotesList.empty();
-
-  // pass `allQuotes` into the template function
-  var quotesHtml = getAllQuotesHtml(allQuotes);
-
-  // append html to the view
-  $quotesList.append(quotesHtml);
-};
+// // helper function to render all posts to view. note: we empty and re-render the collection each time our post data changes
+// function render () {
+//   // empty existing posts from view
+//   $quotesList.empty();
+//
+//   // pass `allQuotes` into the template function
+//   var quotesHtml = getAllQuotesHtml(allQuotes);
+//
+//   // append html to the view
+//   $quotesList.append(quotesHtml);
+// };
 
 
 
