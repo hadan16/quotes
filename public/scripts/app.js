77c082becf13960aa7c310e10a1d980825e183f4
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-02-27 14:23:33 -0800

    updated api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,8 +11,8 @@ $(document).on('ready', function() {
     $(function(){
      $.ajax({
        method: 'GET',
-      //  url: `https://fast-beach-18917.herokuapp.com/api/quotes`,
-       url: `http://localhost:3000/api/quotes`,
+       url: `https://fast-beach-18917.herokuapp.com/api/quotes`,
+      //  url: `http://localhost:3000/api/quotes`,
        dataType: 'json',
        success: onSuccess
      });
@@ -84,7 +84,7 @@ $(document).on('ready', function() {
     // POST request to create new quote
     $.ajax({
       method: "POST", //hit up the api via a post method
-      url: `http://localhost:3000/api/quotes`, //here is the api url
+      url: `https://fast-beach-18917.herokuapp.com/api/quotes`, //here is the api url
       data: newQuote, //grab the serialized data (
                       // ie: task=taskentry1&description=descentry2)
     });
@@ -109,7 +109,7 @@ $(document).on('ready', function() {
           // DELETE request to delete quote
           $.ajax({
             type: 'DELETE',
-            url: `http://localhost:3000/api/quotes` + '/' + quoteId,
+            url: `https://fast-beach-18917.herokuapp.com/api/quotes` + '/' + quoteId,
             success: function () {
               // render (render function requires handlebars)all quote to view
               location.reload();
@@ -145,7 +145,7 @@ $(document).on('ready', function() {
         // PUT request to update quote
         $.ajax({
           type: 'PUT',
-          url: `http://localhost:3000/api/quotes` + '/' + quoteId2,
+          url: `https://fast-beach-18917.herokuapp.com/api/quotes` + '/' + quoteId2,
           data: updatedQuote,
           success: function onUpdateSuccess() {
 
